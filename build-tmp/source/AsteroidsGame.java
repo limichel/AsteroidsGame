45e7e6507225ed86fb45f89b08cf2a882c529ff0
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-11-04 10:41:12 -0800

    Edited SpaceShip class. Added keyboard navigation.

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 SpaceShip ship;
 public void setup() 
 {
@@ -16,11 +32,11 @@ public void keyPressed()
 {
   if(key == '8')
   {
-    ship.accelerate(0.5);
+    ship.accelerate(0.5f);
   }
   if(key == '2')
   {
-    ship.accelerate(-0.5);
+    ship.accelerate(-0.5f);
   }
   if(key == '6')
   {
@@ -152,3 +168,12 @@ abstract class Floater //Do NOT modify the Floater class! Make changes in the Sp
   }   
 } 
 
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
