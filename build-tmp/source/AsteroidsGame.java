c1dad298b59aabf67a83bbd55774b53bcd7bbbb9
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-11-12 11:24:11 -0800

    Added asteroids.

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -16,6 +16,7 @@ public class AsteroidsGame extends PApplet {
 
 SpaceShip ship;
 Star[] stars;
+Asteroid[] asteroids;
 boolean forward = false;
 boolean backward = false;
 boolean clockwise = false;
@@ -26,19 +27,29 @@ public void setup()
   //frameRate(100);
   ship = new SpaceShip();
   stars = new Star[500];
+  asteroids = new Asteroid[7];
   for(int i = 0; i < stars.length; i++)
   {
     stars[i] = new Star();
+  }
+  for(int i = 0; i < asteroids.length; i++)
+  {
+    asteroids[i] = new Asteroid();
   }
 }
 public void draw() 
 {
   background(0);
-  ship.move();
-  ship.show();
   for(int i = 0; i < stars.length; i++)
   {
     stars[i].show();
+  }
+  ship.move();
+  ship.show();
+  for(int i = 0; i < asteroids.length; i++)
+  {
+    asteroids[i].move();
+    asteroids[i].show();
   }
   if(forward == true)
   {
@@ -151,6 +162,70 @@ class SpaceShip extends Floater
   public double getDirectionY(){return myDirectionY;}
   public void setPointDirection(int degrees){myPointDirection = degrees;}
   public double getPointDirection(){return myPointDirection;}
+}
+class Asteroid extends Floater
+{
+  private double myRotationSpeed;
+  public Asteroid()
+  { 
+    corners = 6;
+    xCorners = new int[corners];
+    yCorners = new int[corners];
+    xCorners[0] = 12;
+    yCorners[0] = 0;
+    xCorners[1] = 9;
+    yCorners[1] = 9;
+    xCorners[2] = -3;
+    yCorners[2] = 12;
+    xCorners[3] = -12;
+    yCorners[3] = 0;
+    xCorners[4] = -6;
+    yCorners[4] = -6;
+    xCorners[5] = 3;
+    yCorners[5] = -9;
+    myColor = color(210);
+    myCenterX = (int)(Math.random() * 500);
+    myCenterY = (int)(Math.random() * 500);
+    myDirectionX = 0;
+    myDirectionY = 0;
+    myPointDirection = 0;
+    myRotationSpeed = (Math.random() * 3) - 1;
+  }
+  public void setX(int x){myCenterX = x;}
+  public int getX(){return (int)myCenterX;}
+  public void setY(int y){myCenterY = y;}
+  public int getY(){return (int)myCenterY;}
+  public void setDirectionX(double x){myDirectionX = x;}
+  public double getDirectionX(){return myDirectionX;}
+  public void setDirectionY(double y){myDirectionY = y;}
+  public double getDirectionY(){return myDirectionY;}
+  public void setPointDirection(int degrees){myPointDirection = degrees;}
+  public double getPointDirection(){return myPointDirection;}
+  public void move()
+  {
+    myPointDirection = myPointDirection + myRotationSpeed;
+
+    //change the x and y coordinates by myDirectionX and myDirectionY       
+    myCenterX = myCenterX + myRotationSpeed;    
+    myCenterY = myCenterY + myRotationSpeed;
+    //wrap around screen    
+    if(myCenterX >width)
+    {     
+      myCenterX = 0;    
+    }    
+    else if (myCenterX<0)
+    {     
+      myCenterX = width;    
+    }    
+    if(myCenterY >height)
+    {    
+      myCenterY = 0;    
+    }   
+    else if (myCenterY < 0)
+    {     
+      myCenterY = height;    
+    }   
+  }
 }
 abstract class Floater //Do NOT modify the Floater class! Make changes in the SpaceShip class 
 {   
